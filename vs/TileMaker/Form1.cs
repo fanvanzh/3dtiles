bd9e5594f5cf3df0653ef4f1b55b510dd7f97b93
fanvanzh <fanvanzh@sina.com>
2018-08-26 12:36:57 +0800

    初步完成 模型的转换流程

--- vs/TileMaker/Form1.cs
+++ vs/TileMaker/Form1.cs
@@ -10,15 +10,53 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace TileMaker
 {
     public partial class Form1 : Form
     {
+        public class Asset
+        {
+            public string version;
+            public string gltfUpAxis;
+        };
+        public class Root
+        {
+            public struct BoundingVolume
+            {
+                public double[] box;
+            }
+            public struct Tile {
+                public struct Content {
+                    public string url;
+                }
+                public BoundingVolume boundingVolume;
+                public double geometricError;
+                public Content content;
+            }
+            public BoundingVolume boundingVolume;
+            public double geometricError;
+            public double[] transform;
+            public List<Tile> children;
+        };
+        public class TileSet
+        {
+            public Asset asset;
+            public double geometricError;
+            public Root root;
+        };
+
         [DllImport("tile.dll")]
         public static extern IntPtr osgb23dtile_path(string in_path, string out_path, IntPtr box, ref int len, int max_lvl);
 
         [DllImport("tile.dll")]
         public static extern void free_buffer(IntPtr buffer);
+
+        [DllImport("tile.dll")]
+        public static extern void transform_c(double center_x, double center_y, double height_min, IntPtr ptr);
+        
 
         public Form1()
         {
@@ -104,7 +142,7 @@ namespace TileMaker
             DirectoryInfo[] DirSub = Dir.GetDirectories();
 
             double[] root_box = new double[] { -1.0E+38, -1.0E+38, -1.0E+38, 1.0E+38, 1.0E+38, 1.0E+38 };
-
+            List<string> subtile_json = new List<string>();
             foreach (DirectoryInfo f in DirSub)
             {
                 string osb_sub = f.FullName;
@@ -125,22 +163,94 @@ namespace TileMaker
                         {
                             int buf_len = 0;
                             IntPtr json = osgb23dtile_path(osb_sub, out_path, (IntPtr)ptr, ref buf_len, 18);
-                            string tile_json = Marshal.PtrToStringAnsi(json);
+                            subtile_json.Add(Marshal.PtrToStringAnsi(json));
                             free_buffer(json);
-                            for(int i = 0; i < 3; i++)
-                                  root_box[i] = box[i] > root_box[i] ? box[i] : root_box[i];
+                            for (int i = 0; i < 3; i++)
+                                root_box[i] = box[i] > root_box[i] ? box[i] : root_box[i];
                             for (int i = 3; i < 6; i++)
                                 root_box[i] = box[i] < root_box[i] ? box[i] : root_box[i];
                         }
                     }
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     MessageBox.Show(ex.Message);
                 }
             }
             // 合并外包矩形，合并输出 json
-            string tileset_json = "";
+            string tileset = @"{
+                ""asset"": {
+                ""gltfUpAxis"":""Y"",
+                ""version"": ""0.0""
+                },
+                ""geometricError"": 2000,
+                ""root"": {
+                ""boundingVolume"": {
+                    ""box"": [
+                    0, 0, 0,
+                    0, 0, 0,
+                    0, 0, 0,
+                    0, 0, 0
+                    ]
+                },
+                ""geometricError"": 2000,
+                ""transform"": [
+                    0, 0, 0, 0,
+                    0, 0, 0, 0,
+                    0, 0, 0, 0,
+                    0, 0, 0, 0
+                ]
+                }
+            }";
+            TileSet tilejson = JsonConvert.DeserializeObject<TileSet>(tileset);
+            var bbox = make_tile_box(root_box);
+            for(int i = 0; i < bbox.Count; i++)
+            {
+                tilejson.root.boundingVolume.box[i] = bbox[i];
+            }
+            // write the transform
+            double[] root_transform = new double[16];
+            unsafe
+            {
+                fixed(double* ptr = root_transform)
+                {
+                    transform_c(120, 30, 0, (IntPtr)ptr);
+                }
+            }
+            //JArray transform = (JArray)tilejson["root"]["transform"];
+            for(int i = 0; i < 16; i++)
+            {
+                tilejson.root.transform[i] = root_transform[i];
+            }
+            // add children
+            if(tilejson.root.children == null)
+            {
+                tilejson.root.children = new List<Root.Tile>();
+            }
+            tilejson.root.children.Clear();
+            foreach (var tile in subtile_json)
+            {
+                Root.Tile tileObject = JsonConvert.DeserializeObject<Root.Tile>(tile);
+                tileObject.content.url = tileObject.content.url.Replace("./","./Data/").Replace(".b3dm", "/tileset.json");
+                tilejson.root.children.Add(tileObject);
+
+                string subtile_string = @"{
+                ""asset"": {
+                ""gltfUpAxis"":""Y"",
+                ""version"": ""0.0""
+                },
+                ""root"": ";
+                subtile_string += tile;
+                subtile_string += "}";
+                string tile_path = tileObject.content.url.Replace("./", "/");
+                JObject obj = (JObject)JsonConvert.DeserializeObject(subtile_string);
+                string format_string = JsonConvert.SerializeObject(obj, Formatting.Indented);
+                File.WriteAllText(tile_dir + tile_path, format_string, Encoding.UTF8);
+            }
+            // write root
+            string out_string = JsonConvert.SerializeObject(tilejson, Formatting.Indented);
+            File.WriteAllText(tile_dir + "/tileset.json", out_string, Encoding.UTF8);
+            MessageBox.Show("转换完成！");
         }
 
         private List<double> make_tile_box(double[] box)
--- vs/tile/src/osgb.h
+++ vs/tile/src/osgb.h
@@ -9,4 +9,6 @@ extern "C" {
 	DLL_API void* osgb23dtile_path(const char* in_path, const char* out_path, double *box, int* len, int max_lvl);
 
 	DLL_API void free_buffer(void* buf);
+
+	DLL_API void transform_c(double center_x, double center_y, double height_min, double* ptr);
 }
