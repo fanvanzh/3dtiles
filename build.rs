b1b5f3f2593fb83b72c8da30ce508afcc259c021
Li Yanxu <halyx@126.com>
2025-09-17 10:03:11 +0800

    refactor: unify all platform build process using vcpkg

--- build.rs
+++ build.rs
@@ -1,52 +1,50 @@
-extern crate cc;
+extern crate cmake;
+extern crate pkg_config;
+use cmake::Config;
 use std::env;
 
 fn build_win_msvc() {
-    cc::Build::new()
-        .cpp(true)
-        .flag("-Zi")
-        .flag("-INCREMENTAL")
-        .flag("-bigobj")
-        .warnings(false)
-        .define("WIN32", None)
-        .define("_WINDOWS", None)
-        .include("./src")
-        .include("./vcpkg/installed/x64-windows-release/include")
-        .file("./src/tileset.cpp")
-        .file("./src/shp23dtile.cpp")
-        .file("./src/osgb23dtile.cpp")
-        .file("./src/dxt_img.cpp")
-        .file("./src/GeoTransform.cpp")
-        .compile("_3dtile");
-    // -------------
-    println!("cargo:rustc-link-search=native=./vcpkg/installed/x64-windows-release/lib");
-    // ------ GDAL library -------
-    println!("cargo:rustc-link-lib=gdal");
-    // ------ OSG library --------
+    // Probe Library Link for GDAL and OpenSceneGraph
+    pkg_config::Config::new().atleast_version("3.9.3").probe("gdal").unwrap();
+    pkg_config::Config::new().atleast_version("3.6.5").probe("OpenSceneGraph").unwrap();
+    // Get VCPKG_ROOT environment variable
+    let vcpkg_root = std::env::var("VCPKG_ROOT").unwrap();
+    let dst = Config::new(".").define("CMAKE_TOOLCHAIN_FILE", format!("{}/scripts/buildsystems/vcpkg.cmake", vcpkg_root)).very_verbose(true).build();
+    println!("cmake dst = {}", dst.display());
+    // Link Search Path for C++ Implementation
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    // for FFI C++ static library
+    println!("cargo:rustc-link-lib=static=_3dtile");
+    // Link Search Path for Third Party Libraries
+    let vcpkg_installed_dir = "./vcpkg_installed/x64-windows/lib";
+    println!("cargo:rustc-link-search=native={}", vcpkg_installed_dir);
+    // openscenegraph library
     println!("cargo:rustc-link-lib=osg");
     println!("cargo:rustc-link-lib=osgDB");
     println!("cargo:rustc-link-lib=osgUtil");
-    println!("cargo:rustc-link-lib=osgViewer");
     println!("cargo:rustc-link-lib=OpenThreads");
+    // gdal library
+    println!("cargo:rustc-link-lib=gdal");
 }
 
-fn build_linux_unkonw() {
-    cc::Build::new()
-        .cpp(true)
-        .flag("-std=c++11")
-        .warnings(false)
-        .include("./src")
-        .include("./vcpkg/installed/x64-linux-release/include")
-        .file("./src/tileset.cpp")
-        .file("./src/shp23dtile.cpp")
-        .file("./src/osgb23dtile.cpp")
-        .file("./src/dxt_img.cpp")
-        .file("./src/GeoTransform.cpp")
-        .compile("_3dtile");
-    // -------------
-    println!("cargo:rustc-link-search=native=/usr/lib/x86_64-linux-gnu");
-    println!("cargo:rustc-link-search=native=./vcpkg/installed/x64-linux-release/lib");
-    // -------------
+fn build_linux_unkown() {
+    // Probe Library Link for GDAL and OpenSceneGraph
+    pkg_config::Config::new().atleast_version("3.9.2").probe("gdal").unwrap();
+    pkg_config::Config::new().atleast_version("3.6.5").probe("OpenSceneGraph").unwrap();
+    // Get VCPKG_ROOT environment variable
+    let vcpkg_root = std::env::var("VCPKG_ROOT").unwrap();
+    let dst = Config::new(".").define("CMAKE_TOOLCHAIN_FILE", format!("{}/scripts/buildsystems/vcpkg.cmake", vcpkg_root)).very_verbose(true).build();
+    println!("cmake dst = {}", dst.display());
+    // Link Search Path for C++ Implementation
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    // for FFI C++ static library
+    println!("cargo:rustc-link-lib=static=_3dtile");
+    // for C++ Standard Library
+    println!("cargo:rustc-link-lib=stdc++");
+    // Link Search Path for Third Party Libraries
+    let vcpkg_installed_dir = "./vcpkg_installed/x64-linux-release/lib";
+    println!("cargo:rustc-link-search=native={}", vcpkg_installed_dir);
+    // openscenegraph library
     println!("cargo:rustc-link-lib=osg");
     println!("cargo:rustc-link-lib=osgDB");
     println!("cargo:rustc-link-lib=osgUtil");
@@ -56,41 +54,35 @@ fn build_linux_unkonw() {
 }
 
 fn build_macos() {
-    cc::Build::new()
-        .cpp(true)
-        .flag("-std=c++11")
-        .flag("-Wno-deprecated-declarations")
-        .warnings(false)
-        .include("./src")
-        .include("/opt/homebrew/include")  // Homebrew include path for M1 Mac
-        .include("/opt/homebrew/opt/gdal/include")  // GDAL include path
-        .include("/opt/homebrew/opt/open-scene-graph/include")  // OSG include path
-        .file("./src/tileset.cpp")
-        .file("./src/shp23dtile.cpp")
-        .file("./src/osgb23dtile.cpp")
-        .file("./src/dxt_img.cpp")
-        .file("./src/GeoTransform.cpp")
-        .compile("_3dtile");
-    
-    // Link search paths
-    println!("cargo:rustc-link-search=native=/opt/homebrew/lib");
-    println!("cargo:rustc-link-search=native=/opt/homebrew/opt/gdal/lib");
-    println!("cargo:rustc-link-search=native=/opt/homebrew/opt/open-scene-graph/lib");
-    
-    // OSG libraries
+    // Probe Library Link for GDAL and OpenSceneGraph
+    pkg_config::Config::new().atleast_version("3.9.2").probe("gdal").unwrap();
+    pkg_config::Config::new().atleast_version("3.6.5").probe("OpenSceneGraph").unwrap();
+    // Get VCPKG_ROOT environment variable
+    let vcpkg_root = std::env::var("VCPKG_ROOT").unwrap();
+    let dst = Config::new(".").define("CMAKE_TOOLCHAIN_FILE", format!("{}/scripts/buildsystems/vcpkg.cmake", vcpkg_root)).very_verbose(true).build();
+    println!("cmake dst = {}", dst.display());
+    // Link Search Path for C++ Implementation
+    println!("cargo:rustc-link-search=native={}/lib", dst.display());
+    // for FFI C++ static library
+    println!("cargo:rustc-link-lib=static=_3dtile");
+    // for C++ Standard Library
+    println!("cargo:rustc-link-lib=c++");
+    let vcpkg_installed_dir = "./vcpkg_installed/arm64-osx/lib/";
+    // Link Search Path for Third Party Libraries
+    println!("cargo:rustc-link-search=native={}", vcpkg_installed_dir);
+    // openscenegraph library
     println!("cargo:rustc-link-lib=osg");
     println!("cargo:rustc-link-lib=osgDB");
     println!("cargo:rustc-link-lib=osgUtil");
     println!("cargo:rustc-link-lib=OpenThreads");
-    
-    // GDAL library
+    // gdal library
     println!("cargo:rustc-link-lib=gdal");
 }
 
 fn main() {
     match env::var("TARGET") {
         Ok(val) => match val.as_str() {
-            "x86_64-unknown-linux-gnu" => build_linux_unkonw(),
+            "x86_64-unknown-linux-gnu" => build_linux_unkown(),
             "x86_64-pc-windows-msvc" => build_win_msvc(),
             "aarch64-apple-darwin" => build_macos(),
             &_ => {}
