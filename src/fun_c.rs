19242b5721c6bf7ebc8cbaabacfaa2450168befc
Li Yanxu <halyx@126.com>
2026-02-04 17:19:35 +0800

    fix: issues/380(osgb导出gltf不支持文件名包含路径) (#381)

--- src/fun_c.rs
+++ src/fun_c.rs
@@ -1,12 +1,23 @@
 #[no_mangle]
 pub extern "C" fn write_file(file_name: *const libc::c_char, buf: *const u8, buf_len: u32) -> bool {
     use std::ffi;
+    use std::fs;
     use std::fs::File;
     use std::io::prelude::*;
     use std::slice;
 
     unsafe {
         if let Ok(file_name) = ffi::CStr::from_ptr(file_name).to_str() {
+            use std::path::Path;
+            let path = Path::new(file_name);
+            if let Some(parent) = path.parent() {
+                if !parent.as_os_str().is_empty() {
+                    if let Err(e) = fs::create_dir_all(parent) {
+                        error!("create dir fail: {}", e);
+                        return false;
+                    }
+                }
+            }
             if let Ok(mut f) = File::create(file_name) {
                 let arr = slice::from_raw_parts(buf, buf_len as usize);
                 match f.write_all(arr) {
